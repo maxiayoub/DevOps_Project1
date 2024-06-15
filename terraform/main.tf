09fcd6b3040ea6b633c8b28bb31fccb00436e5bf
Maximous ElKess Ayoub <89272370+maxiayoub@users.noreply.github.com>
2024-06-15 21:08:22 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,9 +18,9 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-#resource "aws_internet_gateway" "igw" { 
- # vpc_id = aws_vpc.main.id
-#}
+resource "aws_internet_gateway" "igw" { 
+  vpc_id = aws_vpc.main.id
+}
 
 resource "aws_subnet" "subnet" {
   vpc_id     = aws_vpc.main.id
